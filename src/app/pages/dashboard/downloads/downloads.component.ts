b9127f4539756046c7ae51f2dd2069d7247a29be
stephen-byte <55480727+stephen-byte@users.noreply.github.com>
2019-10-18 18:28:18 +0900

    modif ts lint

--- src/app/pages/dashboard/downloads/downloads.component.ts
+++ src/app/pages/dashboard/downloads/downloads.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-downloads',
   templateUrl: './downloads.component.html',
-  styleUrls: ['./downloads.component.scss']
+  styleUrls: ['./downloads.component.scss'],
 })
 export class DownloadsComponent implements OnInit {
 
