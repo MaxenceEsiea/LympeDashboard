7d24fbeea38602ac7ca9ac27e3fcb5ee015d9971
Maxence PICOT <42573264+MaxenceEsiea@users.noreply.github.com>
2019-10-20 14:56:47 +0200

    Modifs stats

--- src/app/@theme/components/progress-section/progress-section.component.ts
+++ src/app/@theme/components/progress-section/progress-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import {Component, Input, OnDestroy} from '@angular/core';
 import { ProgressInfo } from '../../../@core/data/stats-progress-bar';
 import {AppService} from '../../../app.service';
 
@@ -11,11 +11,13 @@ export class ECommerceProgressSectionComponent implements OnDestroy {
 
   private alive = true;
 
+  @Input('period') period;
+
   progressInfoData: ProgressInfo[];
 
   constructor(private appService: AppService) {
     this.appService.getProgressData().subscribe((progressData) => {
-      this.progressInfoData = progressData;
+      this.progressInfoData = progressData[this.period];
     });
   }
 
